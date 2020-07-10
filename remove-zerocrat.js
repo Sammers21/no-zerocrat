ea7fb835b4b691efd427bbef7fb9603a6d4ec1e8
Sammers21 <titantins@gmail.com>
2020-07-10 18:37:46 +0300

    initial commit

---
+++ remove-zerocrat.js
@@ -0,0 +1,13 @@
+function getElementsByXPath(xpath, parent)
+{
+    let results = [];
+    let query = document.evaluate(xpath, parent || document,
+        null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    for (let i = 0, length = query.snapshotLength; i < length; ++i) {
+        results.push(query.snapshotItem(i));
+    }
+    return results;
+}
+getElementsByXPath("//div[contains(@class,'timeline-comment-header')]//a[text()='0crat']/../../../..").forEach(elem => elem.remove());
+getElementsByXPath("//a[contains(@data-hovercard-url,'/users/0crat/hovercard')]").forEach(elem => elem.remove());
+console.log("Zerocrat has been removed from the pages");
